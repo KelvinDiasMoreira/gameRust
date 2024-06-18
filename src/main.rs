0d002d06e570bd6f5d0259fe2e0ba89266997693
Kelvi <kelvingremista2015@hotmail.com>
2024-06-17 23:30:04 -0300

    some changes

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use sdl2::{
     pixels::Color,
     rect::{Point, Rect},
     render::Canvas,
-    sys::rand,
+    sys::SDL_atan2,
     video::Window,
 };
 
-use std::time::Duration;
+use std::{f32::consts::PI, time::Duration};
 
 extern crate sdl2;
 
@@ -24,10 +24,12 @@ struct Rectangle2d {
 }
 #[derive(Debug)]
 struct Point2d {
-    position_x: i32,
-    position_y: i32,
+    position_x: f64,
+    position_y: f64,
+    speed_pos_x: f64,
+    speed_pos_y: f64,
     color: Color,
-    initial_position: i32,
+    initial_position: f64,
     is_increase_x: bool,
     is_increase_y: bool,
 }
@@ -41,26 +43,26 @@ impl Rectangle2d {
             if self.position_y != SCREEN_HEIGHT as i32 - self.height as i32 {
                 self.position_y += SPEED_RECTANGLES;
             }
-            self.position_y = point_pos.position_y - (self.height / 2) as i32
+            self.position_y = point_pos.position_y as i32 - (self.height as f64 / 2.0) as i32
         }
     }
 }
 
 impl Point2d {
-    fn movement(&mut self, speed_point_x: i32, speed_point_y: i32) {
+    fn movement(&mut self, speed_point_x: f64, speed_point_y: f64) {
         if self.is_increase_x {
             self.position_x += speed_point_x;
         } else {
             self.position_x -= speed_point_x;
         }
-        if self.position_y < SCREEN_HEIGHT as i32 && self.is_increase_y == true {
-            if self.position_y >= (SCREEN_HEIGHT - speed_point_y as u32) as i32 {
+        if self.position_y < SCREEN_HEIGHT as f64 && self.is_increase_y == true {
+            if self.position_y >= (SCREEN_HEIGHT - speed_point_y as f64) as f64 {
                 self.is_increase_y = false;
             } else {
                 self.position_y += speed_point_y;
             }
         } else {
-            if self.position_y <= (0 + speed_point_y) && !self.is_increase_y {
+            if self.position_y <= (0.0 + speed_point_y) && !self.is_increase_y {
                 self.is_increase_y = true;
             } else {
                 self.position_y -= speed_point_y;
@@ -68,15 +70,21 @@ impl Point2d {
         }
     }
     fn check_colision(&mut self, player1_rectangle: &Rectangle2d, player2_rectangle: &Rectangle2d) {
-        if self.position_x == player1_rectangle.position_x + player1_rectangle.width as i32
-            && self.position_y >= player1_rectangle.position_y
-            && self.position_y <= player1_rectangle.position_y + player1_rectangle.height as i32
+        if self.position_x == player1_rectangle.position_x as f64 + player1_rectangle.width as f64
+            && self.position_y >= player1_rectangle.position_y as f64
+            && self.position_y
+                <= player1_rectangle.position_y as f64 + player1_rectangle.height as f64
         {
+            // let product = self.speed_pos_x * 1.0 + self.speed_pos_y * 0.0;
+            // let vx = self.speed_pos_x - 2.0 * (product * 1.0);
+            // let vy = self.speed_pos_y - 2.0 * (product * 0.0);
+
             self.is_increase_x = true;
         }
-        if self.position_x == player2_rectangle.position_x - player2_rectangle.width as i32
-            && self.position_y >= player2_rectangle.position_y
-            && self.position_y <= player2_rectangle.position_y + player2_rectangle.height as i32
+        if self.position_x == player2_rectangle.position_x as f64 - player2_rectangle.width as f64
+            && self.position_y >= player2_rectangle.position_y.into()
+            && self.position_y
+                <= player2_rectangle.position_y as f64 + player2_rectangle.height as f64
         {
             self.is_increase_x = false;
         }
@@ -84,8 +92,8 @@ impl Point2d {
 }
 
 const SPEED_RECTANGLES: i32 = 10;
-const SCREEN_WIDTH: u32 = 800;
-const SCREEN_HEIGHT: u32 = 600;
+const SCREEN_WIDTH: f64 = 800.0;
+const SCREEN_HEIGHT: f64 = 600.0;
 
 fn main() {
     let speed_point_y: i32 = rand::thread_rng().gen_range(1..15);
@@ -95,7 +103,7 @@ fn main() {
     let video_subsystem = sdl_context.video().unwrap();
 
     let window = video_subsystem
-        .window("Impossivel", SCREEN_WIDTH, SCREEN_HEIGHT)
+        .window("Impossivel", SCREEN_WIDTH as u32, SCREEN_HEIGHT as u32)
         .position_centered()
         .build()
         .expect("failed to open");
@@ -112,10 +120,12 @@ fn main() {
         true,
     );
     let mut point = build_point(
-        SCREEN_WIDTH as i32 / 2,
-        SCREEN_HEIGHT as i32 / 2,
+        SCREEN_WIDTH as f64 / 2.0 as f64,
+        SCREEN_HEIGHT as f64 / 2.0 as f64,
         Color::RGB(255, 255, 255),
-        SCREEN_WIDTH as i32 / 2,
+        SCREEN_WIDTH as f64 / 2.0 as f64,
+        10.0,
+        rand::thread_rng().gen_range(1.0..15.0),
     );
     while game_is_running {
         for event in event_pump.poll_iter() {
@@ -174,7 +184,7 @@ fn update(
     canvas.clear();
 
     canvas.set_draw_color(point.color);
-    let _ = canvas.draw_point(Point::new(point.position_x, point.position_y));
+    let _ = canvas.draw_point(Point::new(point.position_x as i32, point.position_y as i32));
 
     canvas.set_draw_color(player1_rectangle.color);
     let _ = canvas.fill_rect(Rect::new(
@@ -196,8 +206,8 @@ fn update(
         point,
         player1_rectangle,
         player2_rectangle,
-        speed_point_x,
-        speed_point_y,
+        speed_point_x as f64,
+        speed_point_y as f64,
     );
     if player2_rectangle.is_bot {
         player2_rectangle.movement_bot(point);
@@ -209,14 +219,14 @@ fn update_position_point(
     point: &mut Point2d,
     player1_rectangle: &Rectangle2d,
     player2_rectangle: &Rectangle2d,
-    speed_point_x: i32,
-    speed_point_y: i32,
+    speed_point_x: f64,
+    speed_point_y: f64,
 ) {
     point.movement(speed_point_x, speed_point_y);
     point.check_colision(player1_rectangle, player2_rectangle);
-    // if point.position_x == 0 {
-    //     point.position_x = point.initial_position
-    // }
+    if point.position_x == 0.0 {
+        point.position_x = point.initial_position
+    }
 }
 
 fn build_rectangle(
@@ -236,10 +246,19 @@ fn build_rectangle(
     }
 }
 
-fn build_point(position_x: i32, position_y: i32, color: Color, initial_position: i32) -> Point2d {
+fn build_point(
+    position_x: f64,
+    position_y: f64,
+    color: Color,
+    initial_position: f64,
+    speed_pos_x: f64,
+    speed_pos_y: f64,
+) -> Point2d {
     Point2d {
         position_x,
         position_y,
+        speed_pos_x,
+        speed_pos_y,
         color,
         initial_position,
         is_increase_x: false,
